e8122b8e881478108a7b5df38755b7a83cd461cd
applerun <2904185311@qq.com>
2023-06-10 22:26:04 +0800

    Desktop_12900K: db_plot

--- bacteria/code_sjh/utils/RamanData/RamandbFunctions.py
+++ bacteria/code_sjh/utils/RamanData/RamandbFunctions.py
@@ -1,7 +1,10 @@
 import os.path
 import warnings
 from bacteria.code_sjh.Core.RamanData import RamanDatasetCore
+from bacteria.code_sjh.Core.basic_functions import mpl_func
 from matplotlib import pyplot as plt
+import matplotlib.colors as mcolors
+from matplotlib.colors import to_rgba_array, to_rgba
 def save_csv_file_info(Raman:RamanDatasetCore,dst):
     """
     TODO:根据Raman类型在dst处生成对应的信息文件
@@ -24,5 +27,69 @@ def save_csv_file_info(Raman:RamanDatasetCore,dst):
 
 
 
+def db_plot(db: RamanDatasetCore,
+            # svdir = os.path.join(project_root, "Sample_results", "Sample_bad_signal", "plot"),
+            newX = None,  # 是否要插值
+            pltsave_in_one = False,  # 是否保存所有处理后结果在一张图中
+            dir_wise = False,  # True :每个tissue 单独画图，labelwise ：每个label单独画图
+            axes: plt.Axes or list = None,
+            bias = 0,
+            min_bias_interv = 0,
+            ):
+    """
+    database数据可视化
+    @param db: 数据库，RamanDatasetCore
+    @param newX: None 所有光谱的横坐标，默认从db提取
+    @param pltsave_in_one: 是否将所有的数据绘制在同一张图上，若是，将自动设置每个光谱的bias，保证光谱不重叠，自动设置legend（设置在axes外）
+    @param dir_wise: 仅对Raman_dirwise生效，是否每个tissue单独画图
+    @param axes: None，指定光谱绘制的Axes，若绘制在不同的Axes上，需要为Axes的列表
+    @param bias: 将所有曲线向上平移bias
+    @param min_bias_interv: 仅在pltsave_in_one=True时生效，设置不同光谱相对距离最小值
+    @return:
+    """
+    colors = list(mcolors.XKCD_COLORS.keys())
+    if dir_wise:
+        name2data_tissue = db.get_data_sorted_by_sample()  # 按照子文件夹画图
+    else:
+        name2data_tissue = db.get_data_sorted_by_name()  # 选择分类画图
+    if pltsave_in_one:
+        if axes is None:
+            fig, ax = plt.subplots(1, 1)
+        else:
+            fig = axes.figure
+            ax = axes
 
+    else:
+        assert axes is None or type(axes) is list
+        if type(axes) is list:
+            assert len(axes) == db.numclasses
+        fig = None
+    figs = []
+    c = mcolors.XKCD_COLORS[colors[0]]
+    for name in name2data_tissue.keys():
+        tissue_data = name2data_tissue[name].numpy()
+        if pltsave_in_one:
+            c = mcolors.XKCD_COLORS[colors.pop()]
+            mpl_func.spectrum_vis_mpl(tissue_data, db.xs if newX is None else newX, name = name, ax = ax,
+                                      bias = bias,
+                                      title = "processed",
+                                      line_color = to_rgba(c, 1), shadow_color = to_rgba(c, 0.6)
+                                      )
+            bias += max(tissue_data.max(), min_bias_interv)
+        else:
+            fig, ax = plt.subplots(1, 1) if axes is None else axes.pop()
+
+            mpl_func.spectrum_vis_mpl(tissue_data, db.xs if newX is None else newX, name = name, ax = ax,
+                                      bias = bias,
+                                      title = "processed",
+                                      line_color = to_rgba(c, 1), shadow_color = to_rgba(c, 0.6)
+                                      )
+            bias += max(tissue_data.max(), min_bias_interv)
+            figs.append(fig)
+            plt.close(fig)
+    if pltsave_in_one:
+        ax.legend(loc='upper left',bbox_to_anchor=(1.05, 1))
+        return fig
+    else:
+        return figs
 
